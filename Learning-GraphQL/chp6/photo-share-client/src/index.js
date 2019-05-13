db5122cd563b0abe0f67fccc621c0e6f02d22a6d
Tochukwu Nwachukwu <tnwachukwu@realmdigital.co.za>
2019-05-13 21:55:41 +0200

    Appollo Client and React

---
+++ Learning-GraphQL/chp6/photo-share-client/src/index.js
@@ -0,0 +1,36 @@
+import ApolloClient from 'apollo-boost';
+
+const client = new ApolloClient({url: 'http://localhost:40000/graphq'});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
